033b69d742ca62a4e99159809c08276957ba28d8
yavasogluM <40884148+yavasogluM@users.noreply.github.com>
2021-12-27 01:32:48 +0300

    Add files via upload

---
+++ RetailDemo.API/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace RetailDemo.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+
+            services.AddScoped<RetailDemo.Services.Customer.ICustomerService, RetailDemo.Services.Customer.CustomerService>();
+            services.AddScoped<RetailDemo.Services.Invoice.IInvoiceService, RetailDemo.Services.Invoice.InvoiceService>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("ProjectSwagger", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Swagger",
+                    Version = "1.0.0",
+                    Description = "Project's description",
+                });
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(s => { s.SwaggerEndpoint("/swagger/ProjectSwagger/swagger.json", "Swagger"); });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
