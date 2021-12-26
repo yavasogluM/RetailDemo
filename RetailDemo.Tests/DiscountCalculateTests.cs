2bd4ae4cf1f75c49f2f70f8d2b18df477989d4d1
yavasogluM <40884148+yavasogluM@users.noreply.github.com>
2021-12-27 00:35:05 +0300

    Add files via upload

---
+++ RetailDemo.Tests/DiscountCalculateTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using RetailDemo.Models;
+using RetailDemo.Services.Invoice;
+using Xunit;
+
+namespace RetailDemo.Tests
+{
+    public class DiscountCalculateTests
+    {
+        #region Last Calculate
+
+        [Theory]
+        [InlineData(990, 945)]
+        public void CalculateForPrice_SuccessfullTest(decimal totalPrice, decimal expectedResult)
+        {
+            InvoiceService invoiceService = new InvoiceService();
+            var result = invoiceService.CalculateForPrice(totalPrice);
+            Assert.Equal(result, expectedResult);
+        }
+
+        [Theory]
+        [InlineData(990, 960)]
+        public void CalculateForPrice_FailureTest(decimal totalPrice, decimal expectedResult)
+        {
+            InvoiceService invoiceService = new InvoiceService();
+            var result = invoiceService.CalculateForPrice(totalPrice);
+            Assert.NotEqual(result, expectedResult);
+        }
+
+        #endregion Last Calculate
+
+
+        #region Normal Scenario Calculate
+
+        [Fact]
+        public void CalculateDiscount()
+        {
+            CustomerModel customerModel = new CustomerModel { Id = 1, Type = CustomerType.LoyalCustomer };
+            InvoiceModel invoiceModel = new InvoiceModel { Id = 1, Type = InvoiceType.Normal, TotalPrice = 990 };
+            InvoiceService invoiceService = new InvoiceService();
+            var result = invoiceService.CalculateDiscount(customerModel, invoiceModel);
+
+            Assert.Equal(945, result);
+
+        }
+
+        #endregion
+
+
+    }
+}
